0825d110aa83a84204d8427075fdda93138d80d4
Lawrence <56071035+LLLefatle1124@users.noreply.github.com>
2021-10-13 15:54:58 +0200

    Added HIV class with few attributes.

---
+++ Group64/HIV.vb
@@ -0,0 +1,11 @@
+﻿Public Class HIV : Inherits Disease
+
+    Private _noPeopleBornPositive() As Integer
+    Private _noPeopleOnPrep() As Integer
+    Private _noPeopleUndetectable() As Integer
+
+
+    Public Overrides Function Alarm() As String
+        Throw New NotImplementedException()
+    End Function
+End Class
